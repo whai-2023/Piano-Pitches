d66c408b2505d5ff7be57abfdc1304fc6bca968e
Dillon Johnson <dillonj323@gmail.com>
2023-06-29 19:29:14 +1200

    style front page

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -4,14 +4,29 @@ function Home() {
   return (
     <>
       <header className="header">
-        <h1>Piano Pitch!!</h1>
+        <h1 className="title">Piano Pitch!!</h1>
+        <h2 className="subtitle">
+          Welcome to our Whai 2023 choir. Play our piano to hear our beautiful
+          voices.
+        </h2>
       </header>
       <div>
         <Link to={`/page2`}>
-          <button className="searchSubmit">Press Piano Key</button>
+          <button className="button-82-pushable">
+            <span className="button-82-shadow"></span>
+            <span className="button-82-edge"></span>
+            <span className="button-82-front text">Play Piano</span>
+          </button>
+
+          {/* <button className="searchSubmit">Press Piano Key</button> */}
         </Link>
         <Link to={`/page3`}>
-          <button className="searchSubmit">Become A Singer</button>
+          <button className="button-82-pushable">
+            <span className="button-82-shadow"></span>
+            <span className="button-82-edge"></span>
+            <span className="button-82-front text">Become A Singer</span>
+          </button>
+          {/* <button className="searchSubmit">Become A Singer</button> */}
         </Link>
       </div>
     </>
