fb451e0e29d38a195f0eeb9c6e72b26dcbe0366c
martin corpus <nasuntao@yahoo.com>
2023-06-29 14:35:55 +1200

    piano pitches - piano experiment

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,50 +1,74 @@
+// eslint-disable-next-line react-hooks/exhaustive-deps
 import { Link } from 'react-router-dom'
+import { useEffect, useMemo, useCallback } from 'react'
 
 function Home() {
-  const pianoKeys = document.querySelectorAll<HTMLElement>('.piano-keys .key')
+  const pianoKeys = useMemo(
+    () => document.querySelectorAll<HTMLElement>('.piano-keys .key'),
+    []
+  )
   const volumeSlider = document.querySelector<HTMLInputElement>(
     '.volume-slider input'
   )
   const keysCheckbox = document.querySelector<HTMLInputElement>(
     '.keys-checkbox input'
   )
-  const allKeys: string[] = []
-  const audio = new Audio(`tunes/a.wav`) // by default, audio src is "a" tune
+  const allKeys = useMemo(() => {
+    return Array.from(pianoKeys).map((key) => key.dataset.key || '')
+  }, [pianoKeys])
 
-  const playTune = (key: string) => {
-    audio.src = `tunes/${key}.wav` // passing audio src based on key pressed
-    audio.play() // playing audio
-    const clickedKey = document.querySelector<HTMLElement>(
-      `[data-key="${key}"]`
-    ) // getting clicked key element
-    if (clickedKey) {
-      clickedKey.classList.add('active') // adding active class to the clicked key element
-      setTimeout(() => {
-        // removing active class after 150 ms from the clicked key element
-        clickedKey.classList.remove('active')
-      }, 150)
-    }
-  }
+  const audio = useMemo(() => new Audio(`/audio/a.mp3`), [])
+
+  const playTune = useCallback(
+    (key: string) => {
+      audio.src = `/audio/${key}.mp3`
+      audio.play()
+      const clickedKey = document.querySelector<HTMLElement>(
+        `[data-key="${key}"]`
+      )
+
+      if (clickedKey) {
+        clickedKey.classList.add('active')
+        setTimeout(() => {
+          clickedKey.classList.remove('active')
+        }, 150)
+      }
+    },
+    [audio]
+  )
 
   pianoKeys.forEach((key) => {
-    allKeys.push(key.dataset.key || '') // adding data-key value to the allKeys array
-    // calling playTune function with passing data-key value as an argument
+    allKeys.push(key.dataset.key || '')
     key.addEventListener('click', () => playTune(key.dataset.key || ''))
   })
 
   const handleVolume = (e: React.ChangeEvent<HTMLInputElement>) => {
-    audio.volume = Number(e.target.value) // passing the range slider value as an audio volume
+    audio.volume = Number(e.target.value)
   }
 
   const showHideKeys = () => {
-    // toggling hide class from each key on the checkbox click
     pianoKeys.forEach((key) => key.classList.toggle('hide'))
   }
 
-  const pressedKey = (e: KeyboardEvent) => {
-    // if the pressed key is in the allKeys array, only call the playTune function
-    if (allKeys.includes(e.key)) playTune(e.key)
-  }
+  const pressedKey = useCallback(
+    (e: KeyboardEvent) => {
+      console.log('Inside pressedKey function')
+      if (allKeys.includes(e.key)) {
+        playTune(e.key)
+        console.log('Pressed Key:', e.key)
+      }
+    },
+    [allKeys, playTune]
+  )
+
+  useEffect(() => {
+    console.log('Inside useEffect')
+    document.addEventListener('keydown', pressedKey)
+    return () => {
+      console.log('Inside cleanup')
+      document.removeEventListener('keydown', pressedKey)
+    }
+  }, [pressedKey])
 
   if (keysCheckbox) {
     keysCheckbox.addEventListener('click', showHideKeys)
@@ -89,53 +113,74 @@ function Home() {
           <li className="key white" data-key="a">
             <span>a</span>
           </li>
-          <li className="key black" data-key="w">
-            <span>w</span>
+          <li className="key black" data-key="C#">
+            <span>C#</span>
           </li>
-          <li className="key white" data-key="s">
-            <span>s</span>
+          <li className="key white" data-key="D2">
+            <span>D2</span>
           </li>
-          <li className="key black" data-key="e">
-            <span>e</span>
+          <li className="key black" data-key="D#">
+            <span>D#</span>
           </li>
-          <li className="key white" data-key="d">
-            <span>d</span>
+          <li className="key white" data-key="E2">
+            <span>E2</span>
           </li>
-          <li className="key white" data-key="f">
-            <span>f</span>
+          <li className="key white" data-key="F2">
+            <span>F2</span>
           </li>
-          <li className="key black" data-key="t">
-            <span>t</span>
+          <li className="key black" data-key="F#">
+            <span>F#</span>
           </li>
-          <li className="key white" data-key="g">
-            <span>g</span>
+          <li className="key white" data-key="G2">
+            <span>G2</span>
           </li>
-          <li className="key black" data-key="y">
-            <span>y</span>
+          <li className="key black" data-key="G#">
+            <span>G#</span>
           </li>
-          <li className="key white" data-key="h">
-            <span>h</span>
+          <li className="key white" data-key="A2">
+            <span>A2</span>
           </li>
-          <li className="key black" data-key="u">
-            <span>u</span>
+          <li className="key black" data-key="A#">
+            <span>A#</span>
           </li>
-          <li className="key white" data-key="j">
-            <span>j</span>
+          <li className="key white" data-key="B2">
+            <span>B2</span>
           </li>
-          <li className="key white" data-key="k">
-            <span>k</span>
+          <li className="key white" data-key="C3">
+            <span>C3</span>
           </li>
-          <li className="key black" data-key="o">
-            <span>o</span>
+          <li className="key black" data-key="C#">
+            <span>C#</span>
           </li>
-          <li className="key white" data-key="l">
-            <span>l</span>
+          <li className="key white" data-key="D3">
+            <span>D3</span>
           </li>
-          <li className="key black" data-key="p">
-            <span>p</span>
+          <li className="key black" data-key="D#">
+            <span>D#</span>
           </li>
-          <li className="key white" data-key=";">
-            <span>;</span>
+          <li className="key white" data-key="E3">
+            <span>E3</span>
+          </li>
+          <li className="key white" data-key="F3">
+            <span>F3</span>
+          </li>
+          <li className="key black" data-key="F#">
+            <span>F#</span>
+          </li>
+          <li className="key white" data-key="G3">
+            <span>G3</span>
+          </li>
+          <li className="key white" data-key="G#">
+            <span>G#</span>
+          </li>
+          <li className="key black" data-key="A3">
+            <span>A3</span>
+          </li>
+          <li className="key white" data-key="G3">
+            <span>G3</span>
+          </li>
+          <li className="key white" data-key="G3">
+            <span>G3</span>
           </li>
         </ul>
       </div>
