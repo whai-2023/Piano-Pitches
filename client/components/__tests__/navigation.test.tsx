61573f7bd81afb94a6b88918ee766e85320cbe7b
martin corpus <nasuntao@yahoo.com>
2023-07-04 16:05:41 +1200

    piano pitches - navigation test done

--- client/components/__tests__/navigation.test.tsx
+++ client/components/__tests__/navigation.test.tsx
@@ -1,35 +1,45 @@
 // @vitest-environment jsdom
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { renderRoute, screen } from '../../test/utils'
+import readFile from '../../apis/getRandomQuote'
+
+vi.mock('../../apis/getRandomQuote')
 
 describe('Navigation', () => {
-  it('renders navigation links', () => {
+  it('renders navigation links', async () => {
+    vi.mocked(readFile).mockResolvedValue({
+      quote:
+        '`Music is a therapy. It is a communication far more powerful than words, far more immediate, far more efficient.` - Yehudi Menuhin.',
+    })
+
     renderRoute('/')
 
-    const links = screen.getAllByRole('link')
+    const links = await screen.findAllByRole('link')
 
     links.forEach((link) => {
       expect(link).toBeTruthy()
     })
 
     expect(links.map((link) => link.textContent)).toMatchInlineSnapshot(`
-      Array [
+      [
         "Play Piano",
         "Become A Singer",
         "Playground",
       ]
     `)
   })
+})
 
-  it('navigates to the correct routes when clicked', () => {
-    renderRoute('/')
+it('navigates to the correct routes when clicked', () => {
+  renderRoute('/')
 
-    const playPianoLink = screen.getByText('Play Piano')
-    const becomeASingerLink = screen.getByText('Become A Singer')
-    const playgroundLink = screen.getByText('Playground')
-
-    expect(playPianoLink.href).toBe('http://localhost/WhaiPiano') // Use href property to check the value of the href attribute
-    expect(becomeASingerLink.href).toBe('http://localhost/BecomeASinger')
-    expect(playgroundLink.href).toBe('http://localhost/Playground')
+  const playPianoLink = screen.getByRole('link', { name: 'Play Piano' })
+  const becomeASingerLink = screen.getByRole('link', {
+    name: 'Become A Singer',
   })
+  const playgroundLink = screen.getByRole('link', { name: 'Playground' })
+
+  expect(playPianoLink.getAttribute('href')).toBe('/WhaiPiano')
+  expect(becomeASingerLink.getAttribute('href')).toBe('/BecomeASinger')
+  expect(playgroundLink.getAttribute('href')).toBe('/Playground')
 })
