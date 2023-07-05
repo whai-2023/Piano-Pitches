6cf31fc048e0ec3638984d0cd50b658ddf4545f1
martin corpus <nasuntao@yahoo.com>
2023-07-05 16:43:15 +1200

    piano pitches - dokku deployment

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,11 @@ import server from './server'
 
 const PORT = process.env.PORT || 3000
 
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = require('dotenv').config()
+  if (envConfig.error) throw envConfig.error
+}
+
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port', PORT)
